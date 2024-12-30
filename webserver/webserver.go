2d62bf863619601a906a51e10f6aa86349365fd8
Maxim Voynov <maximm.14@yandex.ru>
2024-12-30 14:29:53 +0700

    Update webserver.go

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -11,20 +11,30 @@ import (
 
 func Main(port int, path string) {
     mux := http.NewServeMux()
-    fs := http.FileServer(http.Dir(path))
-    rhf := http.RedirectHandler("/files/", 301)
-    mux.Handle("/files/", http.StripPrefix("/files", fs))
-    mux.Handle("/", rhf)
-
+    mux.HandleFunc("/", pageRoot)
+    mux.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir(path))))
+    
     if *setup.LogLVL <= 1 {
         slog.Info("starting web server on port: " + strconv.Itoa(port))
     }
+
     if err := http.ListenAndServe(":" + strconv.Itoa(port), mux); err != nil {
         if *setup.LogLVL <= 2 {
             slog.Warn(fmt.Sprintf("%v", err))
         }
     }
 }
+
+
+func pageRoot(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	} else {
+        http.Redirect(w, r, "/files/", 301)
+    }    
+}
+
 
 func hardened(handler http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
